5c6debc87e18ceb71b734913bd6c5b58459608e7
Kirill Tapkov <izhjup@mail.ru>
2022-01-26 23:39:16 +0400

    SimpleTextService is written

--- src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleTextService.java
@@ -1,5 +1,7 @@
 package com.epam.izh.rd.online.service;
 
+import java.util.Locale;
+
 public class SimpleTextService implements TextService {
 
     /**
@@ -13,7 +15,7 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public String removeString(String base, String remove) {
-        return null; //TODO
+        return base.replaceAll(remove,"");
     }
 
     /**
@@ -24,7 +26,8 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public boolean isQuestionString(String text) {
-        return false; //TODO
+        char a=text.charAt(text.length()-1);
+        return a=='?';
     }
 
     /**
@@ -35,7 +38,12 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public String concatenate(String... elements) {
-        return null; //TODO
+        StringBuilder str1=new StringBuilder(500);
+        for(String ob : elements) {
+            str1.append(ob);
+            str1.append(" ");
+        }
+        return str1.toString();
     }
 
     /**
@@ -47,7 +55,14 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public String toJumpCase(String text) {
-        return null; //TODO
+        String a=""; //промежуточная строка
+        String jumpString=""; //прыгающая строка
+        for (int i=0; i<text.length(); i++){
+            a+=text.charAt(i);
+            jumpString+= i%2==0 ? a.toLowerCase(Locale.ROOT) : a.toUpperCase(Locale.ROOT);
+            a="";
+        }
+        return jumpString;
     }
 
     /**
@@ -59,6 +74,10 @@ public class SimpleTextService implements TextService {
      */
     @Override
     public boolean isPalindrome(String string) {
-       return false; //TODO
+        string=string.toLowerCase(Locale.ROOT);
+        StringBuilder s1=new StringBuilder(500);
+        s1.append(string);
+        s1.reverse();
+        return s1.toString().contentEquals(string);
     }
 }
